b587f7b73518cee865c46e6ad698b01bb767689b
INT3NSE07 <d.jonathanpaul@gmail.com>
2017-09-30 01:16:09 +0530

    wakeup: Disable some wakelocks
    Change-Id: Idadb42fe46f8f5f4a54fc4b5de9bedf37908eee1

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -23,15 +23,15 @@
 
 static bool enable_qcom_rx_wakelock_ws = false;
 module_param(enable_qcom_rx_wakelock_ws, bool, 0644);
-static bool enable_wlan_extscan_wl_ws = true;
+static bool enable_wlan_extscan_wl_ws = false;
 module_param(enable_wlan_extscan_wl_ws, bool, 0644);
-static bool enable_ipa_ws = true;
+static bool enable_ipa_ws = false;
 module_param(enable_ipa_ws, bool, 0644);
 static bool enable_wlan_ws = true;
 module_param(enable_wlan_ws, bool, 0644);
-static bool enable_timerfd_ws = true;
+static bool enable_timerfd_ws = false;
 module_param(enable_timerfd_ws, bool, 0644);
-static bool enable_netlink_ws = true;
+static bool enable_netlink_ws = false;
 module_param(enable_netlink_ws, bool, 0644);
 
 /*
